e5c8b99f81e58cac03a37ab6629ef6e5a48ac8bf
amouraux <andre.mouraux@uclouvain.be>
2015-05-05 07:04:37 +0200

    Save headers
    Save header now checks for the consistency of events. Deletes duplicate
    events. Deletes events outside of range.

--- core_functions/CLW_save_header.m
+++ core_functions/CLW_save_header.m
@@ -11,6 +11,63 @@ else
     end;
     n=[path filesep n];
 end;
+%!!!!!!
+%!!!!!!
+%check header consistency
+%check for duplicate events
+events=header.events;
+%loop through events
+if length(events)>1;
+    event_index=[];
+    for eventpos=1:length(events);
+        current_code=events(eventpos).code;
+        current_latency=events(eventpos).latency;
+        current_epoch=events(eventpos).epoch;
+        event_list=1:length(events);
+        event_list(eventpos)=[];
+        for eventpos2=1:length(event_list);
+            if strcmpi(events(event_list(eventpos2)).code,current_code);
+                if events(event_list(eventpos2)).epoch==current_epoch;
+                    if current_latency==events(event_list(eventpos2)).latency;
+                        event_index(event_list(eventpos2))=0;
+                    end;
+                end;
+            end;
+        end;
+    end;
+    idx=find(event_index==0);
+    if isempty(idx);
+    else
+        message_string{end+1}=['Deleting ' num2str(length(idx)) ' duplicate events.'];
+        idx=find(event_index==1);
+        events=events(idx);
+    end;
+    %check for invalid events
+    idx=[];
+    for eventpos=1:length(events);
+        if events(eventpos).epoch<0
+            idx(end+1)=eventpos;
+        end;
+        if events(eventpos).epoch>header.datasize(1)
+            idx(end+1)=eventpos;
+        end;
+        if events(eventpos).latency<header.xstart
+            idx(end+1)=eventpos;
+        end;
+        if events(eventpos).latency>header.xstart+((header.datasize(6)-1)*header.xstep)
+            idx(end+1)=eventpos;
+        end;
+    end;
+    if isempty(idx);
+    else
+        idx=unique(idx);
+        message_string{end+1}=['Deleting ' num2str(length(idx)) ' invalid events.'];
+        events(idx)=[];
+    end;
+    header.events=events;
+end;
+%!!!!!
+%!!!!!
 %save header
 save([n '.lw6'],'-MAT','header');
 %filename
