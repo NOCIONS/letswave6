f52a9e21353b8743b3137b873ce5b0e9c3d36973
hughherald <344031924@qq.com>
2015-11-18 18:35:08 +0100

    Fix the events bug in CLW_save

--- core_functions/CLW_save.m
+++ core_functions/CLW_save.m
@@ -20,85 +20,86 @@ if isfield(header,'events');
     events=header.events;
     
     event_num=length(events);
-    event_latency=[events.latency];
-    [~,event_index]=sort(event_latency);
-    event_baned=[];
-    for eventpos=1:event_num
-        if  ismember(eventpos,event_baned)
-            continue;
-        end
-        current_code=events(event_index(eventpos)).code;
-        current_latency=events(event_index(eventpos)).latency;
-        current_epoch=events(event_index(eventpos)).epoch;
-        for eventpos2=eventpos+1:event_num
-            if strcmpi(events(event_index(eventpos2)).code,current_code) &&...
-                    (events(event_index(eventpos2)).epoch==current_epoch) &&...
-                    (current_latency==events(event_index(eventpos2)).latency)
-                event_baned=[event_baned,eventpos2];
-            else
-                break;
+    if event_num~=0
+        event_latency=[events.latency];
+        [~,event_index]=sort(event_latency);
+        event_baned=[];
+        for eventpos=1:event_num
+            if  ismember(eventpos,event_baned)
+                continue;
+            end
+            current_code=events(event_index(eventpos)).code;
+            current_latency=events(event_index(eventpos)).latency;
+            current_epoch=events(event_index(eventpos)).epoch;
+            for eventpos2=eventpos+1:event_num
+                if strcmpi(events(event_index(eventpos2)).code,current_code) &&...
+                        (events(event_index(eventpos2)).epoch==current_epoch) &&...
+                        (current_latency==events(event_index(eventpos2)).latency)
+                    event_baned=[event_baned,eventpos2];
+                else
+                    break;
+                end
             end
         end
+        event_baned=event_index(event_baned);
+        event_baned=[event_baned,find([events.epoch]<0)];
+        event_baned=[event_baned,find([events.epoch]>header.datasize(1))];
+        event_baned=[event_baned,find([events.latency]<header.xstart)];
+        event_baned=[event_baned,find([events.latency]>header.xstart+(header.datasize(6)-1)*header.xstep)];
+        event_baned=unique(event_baned);
+        disp(['Deleting ' num2str(length(event_baned)) ' invalid events.']);
+        header.events=events(setdiff(1:event_num,event_baned));
     end
-    event_baned=event_index(event_baned);
-    event_baned=[event_baned,find([events.epoch]<0)];
-    event_baned=[event_baned,find([events.epoch]>header.datasize(1))];
-    event_baned=[event_baned,find([events.latency]<header.xstart)];
-    event_baned=[event_baned,find([events.latency]>header.xstart+(header.datasize(6)-1)*header.xstep)];
-    event_baned=unique(event_baned);
-    disp(['Deleting ' num2str(length(event_baned)) ' invalid events.']);
-    header.events=events(setdiff(1:event_num,event_baned));
-
-
-%     %loop through events
-%     if length(events)>1;
-%         event_index=ones(size(events));
-%         for eventpos=2:length(events);
-%             current_code=events(eventpos).code;
-%             current_latency=events(eventpos).latency;
-%             current_epoch=events(eventpos).epoch;
-%             event_list=1:eventpos-1;
-%             for eventpos2=1:length(event_list);
-%                 if strcmpi(events(event_list(eventpos2)).code,current_code);
-%                     if events(event_list(eventpos2)).epoch==current_epoch;
-%                         if current_latency==events(event_list(eventpos2)).latency;
-%                             event_index(event_list(eventpos2))=0;
-%                         end;
-%                     end;
-%                 end;
-%             end;
-%         end;
-%         idx=find(event_index==0);
-%         if isempty(idx);
-%         else
-%             disp(['Deleting ' num2str(length(idx)) ' duplicate events.']);
-%             idx=find(event_index==1);
-%             events=events(idx);
-%         end;
-%         %check for invalid events
-%         idx=[];
-%         for eventpos=1:length(events);
-%             if events(eventpos).epoch<0
-%                 idx(end+1)=eventpos;
-%             end;
-%             if events(eventpos).epoch>header.datasize(1)
-%                 idx(end+1)=eventpos;
-%             end;
-%             if events(eventpos).latency<header.xstart
-%                 idx(end+1)=eventpos;
-%             end;
-%             if events(eventpos).latency>header.xstart+((header.datasize(6)-1)*header.xstep)
-%                 idx(end+1)=eventpos;
-%             end;
-%         end;
-%         if isempty(idx);
-%         else
-%             idx=unique(idx);
-%             disp(['Deleting ' num2str(length(idx)) ' invalid events.']);
-%             events(idx)=[];
-%         end;
-%         header.events=events;
-%     end;
+    
+    %     %loop through events
+    %     if length(events)>1;
+    %         event_index=ones(size(events));
+    %         for eventpos=2:length(events);
+    %             current_code=events(eventpos).code;
+    %             current_latency=events(eventpos).latency;
+    %             current_epoch=events(eventpos).epoch;
+    %             event_list=1:eventpos-1;
+    %             for eventpos2=1:length(event_list);
+    %                 if strcmpi(events(event_list(eventpos2)).code,current_code);
+    %                     if events(event_list(eventpos2)).epoch==current_epoch;
+    %                         if current_latency==events(event_list(eventpos2)).latency;
+    %                             event_index(event_list(eventpos2))=0;
+    %                         end;
+    %                     end;
+    %                 end;
+    %             end;
+    %         end;
+    %         idx=find(event_index==0);
+    %         if isempty(idx);
+    %         else
+    %             disp(['Deleting ' num2str(length(idx)) ' duplicate events.']);
+    %             idx=find(event_index==1);
+    %             events=events(idx);
+    %         end;
+    %         %check for invalid events
+    %         idx=[];
+    %         for eventpos=1:length(events);
+    %             if events(eventpos).epoch<0
+    %                 idx(end+1)=eventpos;
+    %             end;
+    %             if events(eventpos).epoch>header.datasize(1)
+    %                 idx(end+1)=eventpos;
+    %             end;
+    %             if events(eventpos).latency<header.xstart
+    %                 idx(end+1)=eventpos;
+    %             end;
+    %             if events(eventpos).latency>header.xstart+((header.datasize(6)-1)*header.xstep)
+    %                 idx(end+1)=eventpos;
+    %             end;
+    %         end;
+    %         if isempty(idx);
+    %         else
+    %             idx=unique(idx);
+    %             disp(['Deleting ' num2str(length(idx)) ' invalid events.']);
+    %             events(idx)=[];
+    %         end;
+    %         header.events=events;
+    %     end;
 else
     header.events=[];
 end
