4ce4bb9106a42b420aa6ba8b7b8b90e510545115
amouraux <amouraux@amouraux-mbp.local>
2016-06-22 06:27:54 +0200

    visualisation glitches

--- GLW/GLW_import_VHDR.m
+++ GLW/GLW_import_VHDR.m
@@ -276,6 +276,3 @@ function prefix_edit_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
-
-
-
